2f62d30dc4cb067672d4b03f78559ad66ff7cb77
Miguel <miguelps95@gmail.com>
2018-06-25 10:01:42 -0500

    nuevo modelo

--- src/espol/edu/ec/main/Main.java
+++ src/espol/edu/ec/main/Main.java
@@ -1,30 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package espol.edu.ec.main;
 
+import java.io.IOException;
+
+import espol.edu.ec.controllers.AddTurnWindow_Controller;
+import espol.edu.ec.controllers.OptionsWindow_Controller;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-/**
- *
- * @author SSAM
- */
-public class Main extends Application{
-    private Pane root;
+public class Main extends Application {
+
+    Stage turnsWindow;
+    Parent rootTurnShower;
+    Parent rootOptions;
+
+
+    @Override
+    public void start(Stage optionsWindow) throws Exception{
+
+        rootOptions = FXMLLoader.load(getClass().getResource("../views/OptionsWindow.fxml"));
+
+        OptionsWindow_Controller controller=new OptionsWindow_Controller();
+
+        optionsWindow.setTitle("Sistema de Migraciones.");
+        optionsWindow.setScene(new Scene(rootOptions));
+        optionsWindow.show();
+
+        initRootTurnShower();
+
+    }
     public static void main(String[] args) {
         launch(args);
     }
-    
-    @Override
-    public void start(Stage stage){
-        root = new Pane();
-        root.setId("Panel");
-        stage.setScene(new Scene(root, 200, 200));
-        stage.show();
+
+    private void initRootTurnShower() throws IOException {
+
+        rootTurnShower= FXMLLoader.load(getClass().getResource("../views/TurnShower.fxml"));
+
+        AddTurnWindow_Controller controller=new AddTurnWindow_Controller();
+        controller.setRootTurnShower(rootTurnShower);
+
+        turnsWindow = new Stage();
+        turnsWindow.setTitle("Mostrador de Turnos.");
+        turnsWindow.setScene(new Scene(rootTurnShower));
+        turnsWindow.show();
+
+    }
+
+    public Parent getRootTurnShower() {
+        return rootTurnShower;
     }
 }
